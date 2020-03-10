d242d8f69d6f60772639d436897860e2e19a306b
xiaosky-007 <61083197+xiaosky-007@users.noreply.github.com>
2020-03-10 19:52:22 +0800

    Update config.default.js

--- egg/config/config.default.js
+++ egg/config/config.default.js
@@ -53,7 +53,7 @@ module.exports = appInfo => {
       //username 
       user: 'root',
       //password 
-      password: '123456',
+      password: '',
       //database 
       database: 'test'
     },
